7bdf15bf89f89fd0a6534be24a206a65f3d5c69d
SxweetLollipop <thaolygoat@gmail.com>
2021-12-14 21:08:47 +0700

    Background elements can gradually change according to external configs (color, transition duration, bubble speed).

--- minesweeper/src/Interface/Background/Background.cpp
+++ minesweeper/src/Interface/Background/Background.cpp
@@ -3,21 +3,21 @@
 #include "Background.h"
 
 
-float dist2Points(const sf::Vector2f p1, const sf::Vector2f p2 = sf::Vector2f(0, 0)) {
+float calDist(const sf::Vector2f p1, const sf::Vector2f p2 = sf::Vector2f(0, 0)) {
 	float x = p1.x - p2.x;
 	float y = p1.y - p2.y;
 	return sqrt(x * x + y * y);
 }
 
 
-Background::Background(const sf::VideoMode window_size, const sf::Color primary_color, const sf::Color secondary_color, const float color_changing_rate, const float circle_speed, const int number_of_circles, const float circle_radius) {
+Background::Background(const sf::VideoMode window_size, const sf::Color primary_color, const sf::Color secondary_color, const float circle_speed, const int number_of_circles, const float circle_radius) {
 
 	this->window_size = window_size;
 
 	cur_prim_color = des_prim_color = primary_color;
 	cur_second_color = des_second_color = secondary_color;
 	this->circle_speed = circle_speed;
-	this->color_changing_rate = color_changing_rate;
+	transition_duration = sf::microseconds(0);
 
 	last_color_update = sf::microseconds(0);
 	last_pos_update = sf::microseconds(0);
@@ -27,74 +27,82 @@ Background::Background(const sf::VideoMode window_size, const sf::Color primary_
 	background.setFillColor(cur_prim_color);
 
 	circles.resize(number_of_circles);
-	for (int i = 0; i < circles.size(); i++) {
-		auto& circle = circles[i];
-		auto& shape = circle.shape;
+	{
+		for (int i = 0; i < circles.size(); i++) {
+			auto& circle = circles[i];
+			auto& shape = circle.shape;
 
-		shape.setRadius(circle_radius);
-		shape.setOrigin(sf::Vector2f(circle_radius, circle_radius));
+			shape.setRadius(circle_radius);
+			shape.setOrigin(sf::Vector2f(circle_radius, circle_radius));
 
-		sf::Color color = secondary_color;
-		color.a = 150;
-		shape.setFillColor(color);
+			sf::Color color = secondary_color;
+			color.a = 150;
+			shape.setFillColor(color);
 
-		{
-			sf::Vector2f pos;
-			pos.x = 1.2 * circle_radius + (rand() % (window_size.width - (int)(circle_radius * 2.2)));
-			pos.y = 1.2 * circle_radius + (rand() % (window_size.height - (int)(circle_radius * 2.2)));
-			shape.setPosition(pos);
-		}
-
-		{
-			auto& veloc = circle.veloc;
-			
-			bool ok = false;
-			while (!ok) {
-				do {
-					veloc.x = ((rand() % 2 == 0) ? -1 : 1) * (1 + rand() % 10000);
-					veloc.y = ((rand() % 2 == 0) ? -1 : 1) * (1 + rand() % 10000);
-					// normalizing
-					float len = sqrt(veloc.x * veloc.x + veloc.y * veloc.y);
-					veloc.x /= len;
-					veloc.y /= len;
-				} while (atan(veloc.x / veloc.y) < ((20 / (float)180) * PI) || atan(veloc.y / veloc.x) < ((20 / (float)180) * PI));
-
-				ok = true;
-				for (int j = 0; j < i && ok; j++) {
-					auto v1 = veloc;
-					v1.x = std::abs(v1.x), v1.y = std::abs(v1.y);
-					auto v2 = circles[j].veloc;
-					v2.x = std::abs(v2.x), v2.y = std::abs(v2.y);
-					auto cos = ((v1.x * v2.x + v1.y * v2.y) / (float)(dist2Points(v1) * dist2Points(v2)));
-
-					//std::cout << std::abs(acos(cos)) << ' ' << ((30 / (float)180) * PI) << '\n';
-					if (std::abs(acos(cos)) < ((20 / (float)180) * PI)) ok = false;
-				}
+			{
+				sf::Vector2f pos;
+				pos.x = 1.2 * circle_radius + (rand() % (window_size.width - (int)(circle_radius * 2.2)));
+				pos.y = 1.2 * circle_radius + (rand() % (window_size.height - (int)(circle_radius * 2.2)));
+				shape.setPosition(pos);
 			}
+		}
+	}
+
+	{
+		float angle = ((90 / (float)(number_of_circles + 1)) / 180) * PI;
+		for(int i = 0; i < circles.size(); i++) {
+			auto& veloc = circles[i].veloc;
+
+			veloc.x = cos(angle * (float)(i + 1));
+			veloc.y = sqrt(1 - veloc.x * veloc.x);
+
+			veloc.x *= ((rand() % 2 == 0) ? -1 : 1);
+			veloc.y *= ((rand() % 2 == 0) ? -1 : 1);
 		}
 	}
 }
 
 
 void Background::calCurrentColor() {
-	auto time_elapsed = (clock.getElapsedTime() - last_color_update).asMilliseconds();
-	float rate = color_changing_rate * (time_elapsed / sf::seconds(1).asMilliseconds());
+	if (transition_duration.asMilliseconds() < 0)
+		return;
 
-	sf::Color color = background.getFillColor();
-	color.r = (des_prim_color.r - color.r) * rate + color.r;
-	color.g = (des_prim_color.r - color.g) * rate + color.g;
-	color.b = (des_prim_color.r - color.b) * rate + color.b;
-	background.setFillColor(color);
+	auto time_elapsed = clock.getElapsedTime() - last_color_update;
+	transition_duration -= time_elapsed;
+	float rate = (1 / (float)transition_duration.asMilliseconds()) * (float)time_elapsed.asMilliseconds();
 
+	{
+		Color color = cur_prim_color;
+		color.r = (des_prim_color.r - color.r) * rate + color.r;
+		color.g = (des_prim_color.g - color.g) * rate + color.g;
+		color.b = (des_prim_color.b - color.b) * rate + color.b;
+
+		if ((des_prim_color.r - color.r) * (des_prim_color.r - cur_prim_color.r) > 0 &&
+			(des_prim_color.g - color.g) * (des_prim_color.g - cur_prim_color.g) > 0 &&
+			(des_prim_color.b - color.b) * (des_prim_color.b - cur_prim_color.b) > 0) {
+
+			cur_prim_color = color;
+		}
+	}
+	background.setFillColor(cur_prim_color);
+
+	{
+		Color color = cur_second_color;
+		color.r = (des_second_color.r - color.r) * rate + color.r;
+		color.g = (des_second_color.g - color.g) * rate + color.g;
+		color.b = (des_second_color.b - color.b) * rate + color.b;
+		color.a = 150;
+
+		if ((des_second_color.r - color.r) * (des_second_color.r - cur_second_color.r) > 0 &&
+			(des_second_color.g - color.g) * (des_second_color.g - cur_second_color.g) > 0 &&
+			(des_second_color.b - color.b) * (des_second_color.b - cur_second_color.b) > 0) {
+
+			cur_second_color = color;
+		}
+	}
 	for (auto& circle : circles) {
 		auto& shape = circle.shape;
-
-		color = shape.getFillColor();
-		color.r = (des_second_color.r - color.r) * rate + color.r;
-		color.g = (des_second_color.r - color.g) * rate + color.g;
-		color.b = (des_second_color.r - color.b) * rate + color.b;
-		color.a = 150;
-		shape.setFillColor(color);
+		shape.setFillColor(cur_second_color);
 	}
 
 	last_color_update = clock.getElapsedTime();
@@ -117,7 +125,7 @@ void Background::calCurrentCirclesPos() {
 	}
 
 	auto time_elapsed = (clock.getElapsedTime() - last_pos_update).asMilliseconds();
-	float distance = (float)circle_speed * (float)(time_elapsed / (float)sf::seconds(1).asMilliseconds());
+	float distance = circle_speed * (time_elapsed / (float)sf::seconds(1).asMilliseconds());
 
 	for (auto& circle : circles) {
 		auto veloc = circle.veloc;
@@ -154,17 +162,28 @@ void Background::update() {
 }
 
 
-void Background::setNextColor(const sf::Color next_prim_color, const sf::Color next_second_color, const sf::Time transition_time) {
+void Background::setNextColor(const sf::Color next_prim_color, const sf::Color next_second_color, const sf::Time transition_duration) {
 	this->des_prim_color = next_prim_color;
+	
 	this->des_second_color = next_second_color;
 
-	auto sec = transition_time.asSeconds();
-	color_changing_rate = 1 / (float)sec;
+	if (transition_duration.asSeconds() > 0) {
+		this->transition_duration = transition_duration;
+		last_color_update = clock.getElapsedTime();
+	}
 }
 
 
 void Background::setCircleSpeed(const float speed) {
-	this->circle_speed = speed;
+	if (speed > 0) {
+		this->circle_speed = speed;
+	}
+}
+
+
+void Background::setNextConfig(const AudioVisualCfg::Cfg& cfg) {
+	setNextColor(cfg.primary_color, cfg.secondary_color, cfg.transition_duration);
+	setCircleSpeed(cfg.bubble_speed);
 }
 
 
